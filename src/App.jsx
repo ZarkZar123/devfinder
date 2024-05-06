6687569356e7ed4317a1a32576c3375bb3021422
Abdul-Razak Adams <abdulrazakadams28@gmail.com>
2024-05-06 18:04:59 +0000

    complete header section

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,21 @@ import './App.css'
 import CardDetails from './components/CardDetails'
 import Header from './components/Header'
 import SearchBar from './components/SearchBar'
+import { useContext } from "react";
+import { ThemeContext } from "./context/ThemeContext";
 
 
 function App() {
+  const {theme}=useContext(ThemeContext)
+
   return (
-  <div className='bg-light-bg flex flex-col justify-center items-center'>
+  <div className={`${ theme==='light'?'bg-light-bg':'bg-d-dark' } flex flex-col justify-center items-center p-[1rem] `}>
     <Header/>
     <SearchBar/>
     <CardDetails/>
     
   </div>
+  
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,20 @@
-import { IoMoonOutline } from "react-icons/io5";
+import {  IoMoonSharp } from "react-icons/io5";
+import { MdSunny } from "react-icons/md";
+import { useContext } from "react";
+import { ThemeContext } from "../context/ThemeContext";
 const Header = () => {
+  const {theme,toggleTheme}=useContext(ThemeContext)
+
   return (
-    <header className="bg-[blue] w-full flex justify-between">
-      <h1>devfinder</h1>
-      <div>
-        <span>Dark</span>
-        <span>
-        <IoMoonOutline />
-        </span>
+    <header className=" w-full md:w-[60%] lg:w-[45%] flex justify-between">
+      <h1 className={` text-lg font-bold text-${theme==='light'?'d-dark':'c-white'} `}>devfinder</h1>
+      <div onClick={toggleTheme}>
+        {
+          theme ==='light'?
+          <span className="flex items-center gap-2 text-c-light-gray "> <span> Dark</span> < IoMoonSharp /> </span>
+          :<span className="flex items-center gap-2 text-c-white "> <span>Light</span> <MdSunny  />  </span> 
+        }
+        
       </div>
     </header>
   )
